8e555e573fbf6f495bde2210da7e82512fc08aab
P-C Markovski <pcwriter@wearingmedia.com>
2022-01-01 13:28:36 +0100

    Added 'readitforme.py.

---
+++ readitforme.py
@@ -0,0 +1,44 @@
+
+
+# Required modules for reading text aloud.
+import pyttsx3
+
+# Required modules for scraping web page data.
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+newVoiceRate = 130 # Set the speech rate.
+
+engine.setProperty('rate',newVoiceRate)
+engine.setProperty('voice', voices[1].id)
+
+# Speak text that is passed as text.
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# Scrape the web page data in div with class 'content-area'.
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    result = soup.find("div", {"class":"content-area"})
+    return result
+
+
+
+
+# Main program starts here.
+
+# Open the URL and return object as context manager.
+html = urllib.request.urlopen('https://www.europezigzag.com/2022/01/find-winter-sun-and-warmth-in-three-european-cities/').read()
+
+# Prase the readable text in particular visual div element.
+txt = text_from_html(html)
+print(txt)
+
+
+# Call the function read the text aloud.
+speak(txt)
